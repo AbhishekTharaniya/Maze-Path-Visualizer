3f9674dabdc7f0a5b30e01f8907fb88b48c2309e
AhishekTharaniya <abhishekthra02@gmail.com>
2023-08-17 00:41:38 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PathFinder from './Components/PathFinder'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      < PathFinder/>
+    </div>
+  )
+}
+
+export default App
